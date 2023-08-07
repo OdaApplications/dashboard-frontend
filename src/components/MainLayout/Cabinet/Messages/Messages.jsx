a89ca28e42ccf1437fbe8c2d7908c4da1503ec58
Nick Leso <nickleso.work@gmail.com>
2023-08-07 20:53:47 +0300

    add pagination markup to messages

--- src/components/MainLayout/Cabinet/Messages/Messages.jsx
+++ src/components/MainLayout/Cabinet/Messages/Messages.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import TablePagination from "@mui/material/TablePagination";
 
 import * as SC from "./Messages.styled";
 import MessageItem from "./MessageItem/MessageItem";
@@ -11,6 +12,8 @@ import { LayoutToolbar } from "components/MainLayout/LayoutToolbar/LayoutToolbar
 
 export const Messages = () => {
   const [userMessages, setUserMessages] = useState([]);
+  const [page, setPage] = useState(2);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
   const { currentData, isFetching, refetch } = useGetUserMsgQuery();
   useEffect(() => {
@@ -21,10 +24,28 @@ export const Messages = () => {
     }
   }, [currentData]);
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
     <>
       <LayoutToolbar>
         <RefreshBtn onClick={refetch} isFetching={isFetching} />
+
+        <TablePagination
+          component="div"
+          count={100}
+          page={page}
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </LayoutToolbar>
       <SC.MessagesContainer>
         <SC.MessagesList>
