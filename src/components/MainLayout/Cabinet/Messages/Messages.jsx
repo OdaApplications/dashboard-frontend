6811d927b7633b72b9b8dbee82a5fc5b1562c03e
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2023-08-29 16:58:39 +0300

    fix

--- src/components/MainLayout/Cabinet/Messages/Messages.jsx
+++ src/components/MainLayout/Cabinet/Messages/Messages.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import * as SC from "./Messages.styled";
 import MessageItem from "./MessageItem/MessageItem";
 import { useGetUserMsgQuery } from "redux/API/cabinetApi";
-
 import { RefreshBtn } from "../../RefreshBtn";
 import { LayoutToolbar } from "components/MainLayout/LayoutToolbar/LayoutToolbar";
 
